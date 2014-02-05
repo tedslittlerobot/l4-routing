7982d8ebc5077ce550d650087623e7d768139a38
Stef Horner <shorner@wearearchitect.com>
2014-02-05 13:21:34 +0000

    add service provider to kick off routing via. events

---
+++ src/RoutesServiceProvider.php
@@ -0,0 +1,46 @@
+<?php namespace Tlr\Routes;
+
+use Illuminate\Support\ServiceProvider;
+
+class RoutesServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Kick off the router using events
+	 * @return [type] [description]
+	 */
+	public function boot()
+	{
+		$startRouter = function() use ( $events )
+		{
+			$events->fire('routes.start', array( $this->app['router'] ));
+		};
+
+		$events->listen('artisan.start', $startRouter);
+		$events->listen('router.before', $startRouter);
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register() {}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
